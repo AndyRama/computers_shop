f50a3c7727106d07292f0f7311e2ba758f6a05e4
AndyRama <andyramaroson@gmail.com>
2021-03-08 16:30:25 +0100

    Add controller |add computer view/show

--- app/controllers/computers_controller.rb
+++ app/controllers/computers_controller.rb
@@ -1,2 +1,11 @@
 class ComputersController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @computers = Computer.all
+  end
+
+  def show
+    @computer = Computer.find(params[:id])
+  end
 end
