5247cf84a2b523522fb52d57d9a096e81b698e57
AndyRama <andyramaroson@gmail.com>
2021-03-13 17:55:50 +0100

    add orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,22 @@
+class OrdersController < ApplicationController
+  def create
+    computer = Computer.find(params[:computer_id])
+    order  = Order.create!(computer: computer, computer_sku: computer.sku, amount: computer.price, state: 'pending', user: current_user)
+
+    session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [{
+        name: computer.sku,
+        images: [computer.photo_url],
+        amount: computer.price_cents,
+        currency: 'eur',
+        quantity: 1
+      }],
+      success_url: order_url(order),
+      cancel_url: order_url(order)
+    )
+
+    order.update(checkout_session_id: session.id)
+    redirect_to new_order_payment_path(order)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'computers#index'
   resources :computers, only: [:index, :show]
+
+  resources :orders, only: [:show, :create]
 end
